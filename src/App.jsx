25120e50952f637c5ce956e878fac4e69e9c2c42
Jean Kennedy <jeankennedyortizvelasquez@gmail.com>
2026-07-18 03:09:26 -0500

    Primer diseño del portafolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,57 @@
 import "./App.css";
 
+/*npm run dev */
+
 function App() {
   return (
-    <>
-      <header>
+    <div className="app">
+
+      <header className="navbar">
         <h2>Jean Kennedy</h2>
 
         <nav>
-          <a href="#about">Sobre mí</a>
-          <a href="#skills">Tecnologías</a>
-          <a href="#projects">Proyectos</a>
-          <a href="#contact">Contacto</a>
+          <a href="#">Sobre mí</a>
+          <a href="#">Tecnologías</a>
+          <a href="#">Proyectos</a>
+          <a href="#">Contacto</a>
         </nav>
       </header>
 
-      <main>
+      <main className="hero">
 
-        <section className="hero">
+        <p className="small">
+          Bienvenido a mi portafolio
+        </p>
 
-          <h1>
-            Hola, soy <span>Jean Kennedy</span>
-          </h1>
+        <h1>
+          Hola, soy
+          <span> Jean Kennedy</span>
+        </h1>
 
-          <p>
-            Desarrollador de Software (No quiero trabajar).
-          </p>
+        <h2>
+          Intento de Desarrollador 
+        </h2>
 
-          <button>
+        <p className="description">
+          Desarrollando aplicaciones web modernas utilizando
+          Java, Spring Boot, React y MySQL.
+        </p>
+
+        <div className="buttons">
+
+          <button className="primary">
             Ver proyectos
           </button>
 
-        </section>
+          <button className="secondary">
+            Descargar CV
+          </button>
+
+        </div>
 
       </main>
-    </>
+
+    </div>
   );
 }
 
